83160c6bb35c29ad534e3fe22210e0017136c11e
Lap Pham <lapphamsf@gmail.com>
2024-07-08 19:05:35 -0700

    update abit

--- main.py
+++ main.py
@@ -7,13 +7,27 @@ app= Flask (__name__)
 proxied =FlaskBehindProxy(app)
 
 
-@app.route('/', methods=['GET','POST'])
+
 @app.route('/register', methods=['GET', 'POST'])
 def regiser():
     return render_template('register.html')
+
+
+# home page login and register 
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
+
+@app.route('/check', methods=['POST'])
+def check():
+    if request.request== 'POST':
+        username=request.form['username']
+        password=request.form['password']
+        #find the data in the sql. if match we render the page
+        
+        #return load page
+        
 
 if __name__ == '__main__':
     app.run(debug=True, host ='0.0.0.0')
