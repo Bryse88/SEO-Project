164c97ccb3be8fb10967aa8bb7af0b9b64cac539
lap1597 <lapphamsf@gmail.com>
2024-07-09 12:26:09 -0700

    test

--- main.py
+++ main.py
@@ -2,32 +2,23 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 
-app= Flask (__name__)
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = 'cb4c4239a7c627143c706956f936a182'
 
-proxied =FlaskBehindProxy(app)
-
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def regiser():
-    return render_template('register.html')
-
-
-# home page login and register 
-@app.route('/', methods=['GET','POST'])
+# Home page with login and register links``
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('main.html', title='Main Page')
 
-
-@app.route('/check', methods=['POST'])
-def check():
-    if request.request== 'POST':
-        username=request.form['username']
-        password=request.form['password']
-        #find the data in the sql. if match we render the page
-        
-        #return load page
-        
+# Register page with form
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True, host ='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
