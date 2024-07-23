9be3a5dc8495cb46ba3726bd661f09f342abb56d
adamBataineh <138632403+adamBataineh@users.noreply.github.com>
2024-07-23 13:26:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,23 +36,6 @@ google = oauth.remote_app(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
 )
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    notes = db.relationship('Note', backref='author', lazy=True)
-    collaborations = db.relationship('Collaboration', backref='collaborator', lazy=True)
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    collaborations = db.relationship('Collaboration', backref='note', lazy=True)
-
-class Collaboration(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    note_id = db.Column(db.Integer, db.ForeignKey('note.id'), nullable=False)
-    collaborator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
 @app.before_request
 def before_request():
     if not hasattr(app, 'db_initialized'):
